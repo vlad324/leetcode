9976a419ac1846e48ebdb537496ba4be8853a12b
vlad324 <uladzislau.radkevich@gmail.com>
2021-04-04 19:56:10 +0200

    Task #622 updated test

--- src/test/java/io/github/vlad324/n622/MyCircularQueueTest.java
+++ src/test/java/io/github/vlad324/n622/MyCircularQueueTest.java
@@ -9,31 +9,47 @@ class MyCircularQueueTest {
     @Test
     void should_produce_expected_result() {
         // given
-        final var myCircularQueue = new MyCircularQueue(3);
+        final var queue = new MyCircularQueue(3);
 
         // then
-        assertThat(myCircularQueue.deQueue()).isFalse();
-        assertThat(myCircularQueue.Rear()).isEqualTo(-1);
-        assertThat(myCircularQueue.Front()).isEqualTo(-1);
-        assertThat(myCircularQueue.isFull()).isFalse();
-        assertThat(myCircularQueue.isEmpty()).isTrue();
+        assertThat(queue.deQueue()).isFalse();
+        assertThat(queue.Rear()).isEqualTo(-1);
+        assertThat(queue.Front()).isEqualTo(-1);
+        assertThat(queue.isFull()).isFalse();
+        assertThat(queue.isEmpty()).isTrue();
 
 
-        assertThat(myCircularQueue.enQueue(1)).isTrue();
-        assertThat(myCircularQueue.Rear()).isEqualTo(1);
-        assertThat(myCircularQueue.Front()).isEqualTo(1);
-        assertThat(myCircularQueue.isFull()).isFalse();
-        assertThat(myCircularQueue.isEmpty()).isFalse();
+        assertThat(queue.enQueue(1)).isTrue();
+        assertThat(queue.Rear()).isEqualTo(1);
+        assertThat(queue.Front()).isEqualTo(1);
+        assertThat(queue.isFull()).isFalse();
+        assertThat(queue.isEmpty()).isFalse();
 
 
-        assertThat(myCircularQueue.enQueue(2)).isTrue();
-        assertThat(myCircularQueue.enQueue(3)).isTrue();
-        assertThat(myCircularQueue.enQueue(4)).isFalse();
-        assertThat(myCircularQueue.Rear()).isEqualTo(3);
-        assertThat(myCircularQueue.isFull()).isTrue();
-        assertThat(myCircularQueue.deQueue()).isTrue();
-        assertThat(myCircularQueue.enQueue(4)).isTrue();
-        assertThat(myCircularQueue.Rear()).isEqualTo(4);
-        assertThat(myCircularQueue.Front()).isEqualTo(2);
+        assertThat(queue.enQueue(2)).isTrue();
+        assertThat(queue.Rear()).isEqualTo(2);
+        assertThat(queue.Front()).isEqualTo(1);
+        assertThat(queue.isFull()).isFalse();
+        assertThat(queue.isEmpty()).isFalse();
+
+        assertThat(queue.enQueue(3)).isTrue();
+        assertThat(queue.Rear()).isEqualTo(3);
+        assertThat(queue.Front()).isEqualTo(1);
+        assertThat(queue.isFull()).isTrue();
+        assertThat(queue.isEmpty()).isFalse();
+
+        assertThat(queue.enQueue(4)).isFalse();
+
+        assertThat(queue.deQueue()).isTrue();
+        assertThat(queue.Rear()).isEqualTo(3);
+        assertThat(queue.Front()).isEqualTo(2);
+        assertThat(queue.isFull()).isFalse();
+        assertThat(queue.isEmpty()).isFalse();
+
+        assertThat(queue.enQueue(4)).isTrue();
+        assertThat(queue.Rear()).isEqualTo(4);
+        assertThat(queue.Front()).isEqualTo(2);
+        assertThat(queue.isFull()).isTrue();
+        assertThat(queue.isEmpty()).isFalse();
     }
 }
