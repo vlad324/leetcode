20993544208992dac60052ee4a00c1ab4331a2b8
vlad324 <uladzislau.radkevich@gmail.com>
2021-04-05 22:01:55 +0200

    Task #775 updated 2

--- src/main/java/io/github/vlad324/n775/Solution.java
+++ src/main/java/io/github/vlad324/n775/Solution.java
@@ -5,30 +5,13 @@ package io.github.vlad324.n775;
  */
 class Solution {
     public boolean isIdealPermutation(int[] A) {
-        int local = 0;
-        int[] positions = new int[A.length];
         for (int i = 0; i < A.length; i++) {
-            if (i < A.length - 1 && A[i] > A[i + 1]) {
-                local++;
-            }
-
-            positions[A[i]] = i;
-        }
-
-        int global = 0;
-        for (int i = 0; i < positions.length; i++) {
-            global += positions[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if (positions[j] < positions[i]) {
-                    global--;
-                }
-            }
-
-            if (global > local) {
+            final var d = A[i] - i;
+            if (d > 1 || d < -1) {
                 return false;
             }
         }
 
-        return global == local;
+        return true;
     }
 }
