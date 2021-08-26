55170bf886a145aafd30fefd124c2427c5ebea91
vlad324 <uladzislau.radkevich@gmail.com>
2021-08-26 21:11:16 +0200

    Task #283 updated

--- src/main/java/io/github/vlad324/n283/Solution.java
+++ src/main/java/io/github/vlad324/n283/Solution.java
@@ -1,20 +1,21 @@
 package io.github.vlad324.n283;
 
-import java.util.ArrayDeque;
-
 /**
  * {@link "https://leetcode.com/problems/move-zeroes/"}
  */
 class Solution {
     public void moveZeroes(int[] nums) {
-        final var zeroIndexes = new ArrayDeque<Integer>();
+        int zIndex = -1;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeroIndexes.add(i);
-            } else if (!zeroIndexes.isEmpty()) {
-                nums[zeroIndexes.poll()] = nums[i];
+            if (nums[i] == 0 && zIndex == -1) {
+                zIndex = i;
+                continue;
+            }
+
+            if (nums[i] != 0 && zIndex != -1) {
+                nums[zIndex] = nums[i];
                 nums[i] = 0;
-                zeroIndexes.add(i);
+                zIndex++;
             }
         }
     }
