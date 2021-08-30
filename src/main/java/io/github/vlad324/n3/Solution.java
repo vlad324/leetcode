045d4c9811cb4337132713c67edc08c51038dbf5
vlad324 <uladzislau.radkevich@gmail.com>
2021-08-30 21:05:05 +0200

    Task #3 updated

--- src/main/java/io/github/vlad324/n3/Solution.java
+++ src/main/java/io/github/vlad324/n3/Solution.java
@@ -1,30 +1,24 @@
 package io.github.vlad324.n3;
 
-import java.util.HashSet;
-
 /**
  * {@link "https://leetcode.com/problems/longest-substring-without-repeating-characters/"}
  */
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null) {
-            return 0;
-        }
-
         int max = 0;
-        final var seen = new HashSet<Character>();
 
-        int start = 0;
-        int end = 0;
-        while (end < s.length()) {
-            final var lastChar = s.charAt(end);
-            if (seen.contains(lastChar)) {
-                seen.remove(s.charAt(start));
-                start++;
+        int i = 0;
+        int j = 0;
+        final var seen = new boolean[127];
+        while (j < s.length()) {
+            final var c = s.charAt(j);
+            if (seen[c]) {
+                seen[s.charAt(i)] = false;
+                i++;
             } else {
-                seen.add(lastChar);
-                end++;
-                max = Math.max(max, seen.size());
+                seen[c] = true;
+                j++;
+                max = Math.max(max, j - i);
             }
         }
 
