bb6d2ca5b5991f02fb550efbf3b271553dbe03e4
vlad324 <uladzislau.radkevich@gmail.com>
2021-11-22 19:30:14 +0100

    Task #450 updated

--- src/main/java/io/github/vlad324/n450/Solution.java
+++ src/main/java/io/github/vlad324/n450/Solution.java
@@ -2,60 +2,39 @@ package io.github.vlad324.n450;
 
 import io.github.vlad324.common.TreeNode;
 
-import java.util.LinkedList;
-
 /**
  * {@link "https://leetcode.com/problems/delete-node-in-a-bst/"}
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        final var holder = new TreeNode(-1, root, null);
-        final var queue = new LinkedList<TreeNode>();
-        queue.add(holder);
-        while (!queue.isEmpty()) {
-            final var current = queue.poll();
-            if (current == null) {
-                continue;
-            }
-
-            if (current.left != null && current.left.val == key) {
-                current.left = delete(current.left);
-                break;
-            }
-
-            if (current.right != null && current.right.val == key) {
-                current.right = delete(current.right);
-                break;
-            }
-
-            queue.add(current.left);
-            queue.add(current.right);
-        }
-
-        return holder.left;
-    }
-
-    private TreeNode delete(TreeNode root) {
-        if (root.left == null && root.right == null) {
+        if (root == null) {
             return null;
         }
 
-        if (root.left != null && root.right != null) {
-            var prev = root;
-            var node = root.right;
-            while (node.left != null) {
-                prev = node;
-                node = node.left;
-            }
+        if (root.val == key) {
+            if (root.left != null && root.right != null) {
+                var node = root.right;
+                while (node.left != null) {
+                    node = node.left;
+                }
 
-            if (prev != root) {
-                prev.left = node.right;
-                node.right = root.right;
+                root.right = deleteNode(root.right, node.val);
+                root.val = node.val;
+
+                return root;
+            } else if (root.left != null) {
+                return root.left;
+            } else {
+                return root.right;
             }
-            node.left = root.left;
-            return node;
         }
 
-        return root.left != null ? root.left : root.right;
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+
+        return root;
     }
 }
