a03872c2977464bca7aa32acf56b1c5c1f4d13a1
vlad324 <uladzislau.radkevich@gmail.com>
2021-12-05 11:10:12 +0100

    Task #55 updated

--- src/main/java/io/github/vlad324/n55/Solution.java
+++ src/main/java/io/github/vlad324/n55/Solution.java
@@ -1,33 +1,18 @@
 package io.github.vlad324.n55;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * {@link "https://leetcode.com/problems/jump-game/"}
  */
 class Solution {
     public boolean canJump(int[] nums) {
-        return canJump(0, nums, new HashSet<>());
-    }
-
-    public boolean canJump(int i, int[] nums, Set<Integer> visited) {
-        if (visited.contains(i)) {
-            return false;
-        } else if (i == nums.length - 1) {
-            return true;
-        } else if (i >= nums.length) {
-            return false;
-        }
-
-        for (int j = nums[i]; j > 0; j--) {
-            if (canJump(i + j, nums, visited)) {
-                return true;
+        int last = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // if it can jump from this index to the last
+            if (i + nums[i] >= last) {
+                last = i;
             }
         }
 
-        visited.add(i);
-
-        return false;
+        return last == 0;
     }
 }
